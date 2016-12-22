6dfc4b8b6f79a33cb9f923449858554ecc137e7c
Shin Shinman <studio@orkana39.pl>
2016-12-22 16:55:31 +0100

    Dla mediów – setup: strony i DT

---
+++ data-sources/data.media_contact.php
@@ -0,0 +1,67 @@
+<?php
+class datasourcemedia_contact extends SectionDatasource
+{
+    public $dsParamROOTELEMENT = 'media-contact';
+    public $dsParamORDER = 'desc';
+    public $dsParamPAGINATERESULTS = 'no';
+    public $dsParamLIMIT = '20';
+    public $dsParamSTARTPAGE = '1';
+    public $dsParamREDIRECTONEMPTY = 'no';
+    public $dsParamREDIRECTONFORBIDDEN = 'no';
+    public $dsParamREDIRECTONREQUIRED = 'no';
+    public $dsParamSORT = 'system:id';
+    public $dsParamHTMLENCODE = 'no';
+    public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+
+    public $dsParamINCLUDEDELEMENTS = array(
+        'title: formatted',
+        'article: formatted'
+    );
+    
+    public function __construct($env = null, $process_params = true)
+    {
+        parent::__construct($env, $process_params);
+        $this->_dependencies = array();
+    }
+    public function about()
+    {
+        return array(
+            'name' => 'Media Contact',
+            'author' => array(
+                'name' => 'Olaf Schindler',
+                'website' => 'http://localhost/ma.wroc.pl',
+                'email' => 'studio@orkana39.pl'),
+            'version' => 'Symphony 2.6.3',
+            'release-date' => '2016-12-22T15:32:01+00:00'
+        );
+    }
+    public function getSource()
+    {
+        return '58';
+    }
+    public function allowEditorToParse()
+    {
+        return true;
+    }
+    public function execute(array &$param_pool = null)
+    {
+        $result = new XMLElement($this->dsParamROOTELEMENT);
+        try{
+            $result = parent::execute($param_pool);
+        } catch (FrontendPageNotFoundException $e) {
+            // Work around. This ensures the 404 page is displayed and
+            // is not picked up by the default catch() statement below
+            FrontendPageNotFoundExceptionHandler::render($e);
+        } catch (Exception $e) {
+            $result->appendChild(new XMLElement('error', $e->getMessage() . ' on ' . $e->getLine() . ' of file ' . $e->getFile()));
+            return $result;
+        }
+        if ($this->_force_empty_result) {
+            $result = $this->emptyXMLSet();
+        }
+        if ($this->_negate_result) {
+            $result = $this->negateXMLSet();
+        }
+        return $result;
+    }
+}
