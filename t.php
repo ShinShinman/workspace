8d4054cc42d2ade9cbc07e5e68f0700de1f455e6
ShinShinman <studio@orkana39.pl>
2020-12-08 15:17:59 +0100

    Kolekcja – tunel z serwerem MA  * strona workspace/t.php  * ilustracje na collection_item z tunelu  * audiodeskrypcja z tunelu  * loightbox z tunelu

---
+++ t.php
@@ -0,0 +1,16 @@
+<?php
+$url='http://156.17.251.36:59190/' . $_GET['link'];
+$content_type=get_headers($url, 1)["Content-Type"];
+$ch=curl_init();
+$timeout=5;
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+// curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+$result=curl_exec($ch);
+curl_close($ch);
+header('Content-type: ' . $content_type);
+echo $result;
+?>
